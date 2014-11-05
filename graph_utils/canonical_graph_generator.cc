215a0e7af6e4d86947d9b5c3f909d1b3bf6ad770
Radko Kotev <radkokotev@gmail.com>
2014-11-05 09:56:33 +0000

    Added skeleton for canonical generation

---
+++ graph_utils/canonical_graph_generator.cc
@@ -0,0 +1,30 @@
+
+#include "graph.h"
+
+namespace graph_utils {
+
+explicit CanonicalGraphGenerator::CanonicalGraphGenerator(const int n) {
+  // TODO(radkokotev) provide implementation.
+}
+
+void CanonicalGraphGenerator::GenerateUpperObjects(
+    const Graph &g, vector<Graph *> *upper_obj) {
+  // TODO(radkokotev) provide implementation.
+}
+
+void CanonicalGraphGenerator::GenerateLowerObjects(
+    const Graph &g, vector<Graph *> *lower_obj) {
+  // TODO(radkokotev) provide implementation.
+}
+
+void GetAllRelatedLowerObjects(const Graph &g,
+                               vector<Graph *> *lower_obj) {
+  // TODO(radkokotev) provide implementation.
+}
+
+void FindGraphsFromLowerObject(const Graph &lower_obj,
+                               vector<Graph *> *graphs) {
+  // TODO(radkokotev) provide implementation.
+}
+
+} // namespace graph_utils
