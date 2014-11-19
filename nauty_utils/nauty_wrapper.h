86442ff2242d267d27f28d61a9d81e6068487a22
Radko Kotev <radkokotev@gmail.com>
2014-11-19 19:31:49 +0000

    Added better filter to the isomorph checker.

--- nauty_utils/nauty_wrapper.h
+++ nauty_utils/nauty_wrapper.h
@@ -3,6 +3,7 @@
 #define NAUTY_UTILS_NAUTY_WRAPPER_H_
 
 #include <map>
+#include <string>
 #include <vector>
 #include "graph_utils/graph.h"
 
@@ -24,7 +25,7 @@ public:
 private:
   bool optimize_;
   vector<Graph *> graphs_;
-  map<int, vector<Graph *> > degree_to_graphs_;
+  map<std::string, vector<Graph *> > degree_to_graphs_;
 };
 
 } // namespace nauty_utils
